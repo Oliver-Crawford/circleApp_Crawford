44802cdbf39bbd9cb951209e79cc0ffb4648cfe3
Oliver Crawford <o0561319@amarillocollege.com>
2023-05-04 18:06:04 -0500

    Initial Commit

---
+++ circleApp_Crawford/circleApp_CrawfordApp.swift
@@ -0,0 +1,17 @@
+//
+//  circleApp_CrawfordApp.swift
+//  circleApp_Crawford
+//
+//  Created by AC Student on 5/4/23.
+//
+
+import SwiftUI
+
+@main
+struct circleApp_CrawfordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
