a302bd8c65e65b2c6915ac7f1261dd690a417d84
Oliver Crawford <o0561319@amarillocollege.com>
2023-05-04 19:18:03 -0500

    committing all changes

--- circleApp_Crawford/ContentView.swift
+++ circleApp_Crawford/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    let circleLineWidth = 100
+    let circleWidthHeight = 250
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Circle()
+                .trim(from: 0.75, to: 1.0)
+                .stroke(Color(.systemGreen), lineWidth: CGFloat(circleLineWidth+20))
+                .frame(width: CGFloat(circleWidthHeight), height: CGFloat(circleWidthHeight))
+                .shadow(color:.gray, radius: CGFloat(10), x:0, y:0)
+                .overlay(alignment: .topTrailing) {
+                    Text("25%")
+                        .font(.system(size:40))
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.white)
+                }
+            Circle()
+                .trim(from: 0.0, to: 0.4)
+                .stroke(Color(.systemBlue), lineWidth: CGFloat(circleLineWidth))
+                .frame(width: CGFloat(circleWidthHeight), height: CGFloat(circleWidthHeight))
+            Circle()
+                .trim(from: 0.4, to: 0.6)
+                .stroke(Color(.systemOrange), lineWidth: CGFloat(circleLineWidth))
+                .frame(width: CGFloat(circleWidthHeight), height: CGFloat(circleWidthHeight))
+            Circle()
+                .trim(from: 0.6, to: 0.75)
+                .stroke(Color(.systemRed), lineWidth: CGFloat(circleLineWidth))
+                .frame(width: CGFloat(circleWidthHeight), height: CGFloat(circleWidthHeight))
         }
-        .padding()
     }
 }
 
